37d15c6a1551e9e5cea87664acf1fcd9de13b50a
Blake <macdadeblake@gmail.com>
2021-08-08 14:32:29 +1200

    fixed the creation type of the database and added addHandler method

--- app/src/main/java/com/example/antitime_wasting/DBHandler.kt
+++ app/src/main/java/com/example/antitime_wasting/DBHandler.kt
@@ -13,12 +13,31 @@ class DBHandler(context: Context?, name: String?, factory: CursorFactory?, versi
     override fun onCreate(db: SQLiteDatabase?) {
         var CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + "("
         CREATE_TABLE += COLUMN_ID + " INTEGER PRIMARY KEY NOT NULL, " +
-                COLUMN_START_TIME + " timestamp, " +
-                COLUMN_END_TIME + " timestamp)"
+                COLUMN_START_TIME + " INTEGER, " +
+                COLUMN_END_TIME + " INTEGER, " +
+                COLUMN_SESSION_TYPE+ " TEXT " +
+                ")"
         if (db != null) {
             db.execSQL(CREATE_TABLE)
         }
     }
+
+    fun addHandler(session: Session) {
+        try {
+            val values = ContentValues()
+            values.put(COLUMN_ID, session.id)
+            values.put(COLUMN_START_TIME, session.startTime)
+            values.put(COLUMN_END_TIME, session.endTime)
+            values.put(COLUMN_SESSION_TYPE, session.sessionType)
+            val db = this.writableDatabase
+            db.insert(TABLE_NAME, null, values)
+            db.close()
+        } catch (e: SQLiteConstraintException) {
+            Log.d(debugTag, "Got SQLiteConstraintException in addHandler method of DBHandler")
+        }
+    }
+
+
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
         TODO("Not yet implemented")
