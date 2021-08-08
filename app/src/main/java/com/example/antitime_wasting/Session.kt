8d58a6e5154870fee065772a1fb90f60f1c32d6f
Blake <macdadeblake@gmail.com>
2021-08-08 14:38:47 +1200

    filled out Session.kt to have getter and setter methods and constructo

--- app/src/main/java/com/example/antitime_wasting/Session.kt
+++ app/src/main/java/com/example/antitime_wasting/Session.kt
@@ -7,11 +7,42 @@ package com.example.antitime_wasting
 */class Session {
 
     var id = 0
+        private set
 
     var startTime:Int? = null
+        private set
 
     var endTime:Int? = null
+        private set
 
     var sessionType:String? = null
+        private set
+
+    constructor(id:Int,start:Int,end:Int,sessionType:String){
+        this.id = id
+        this.startTime = start
+        this.endTime = end
+        this.sessionType = sessionType
+    }
+
+    constructor(id:Int){
+        this.id = id
+    }
+
+    fun setID(ID:Int){
+        this.id = ID
+    }
+
+    fun setStart(start:Int){
+        this.startTime = start
+    }
+
+    fun setEnd(end:Int){
+        this.endTime = end
+    }
+
+    fun setSessionType(session:String){
+        this.sessionType = session
+    }
 
 }
