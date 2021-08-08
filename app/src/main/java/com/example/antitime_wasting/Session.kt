1da48988bfe569f2ecbb676a2a2c667d9385001e
Blake <macdadeblake@gmail.com>
2021-08-08 14:22:51 +1200

    added session object to represent a study session

---
+++ app/src/main/java/com/example/antitime_wasting/Session.kt
@@ -0,0 +1,17 @@
+package com.example.antitime_wasting
+
+/*
+* @created 08/Aug/2021 - 2:20 PM
+* @project Anti time-wasting
+* @author Blake MacDade
+*/class Session {
+
+    var id = 0
+
+    var startTime:Int? = null
+
+    var finishTime:Int? = null
+
+    var sessionType:String? = null
+
+}
