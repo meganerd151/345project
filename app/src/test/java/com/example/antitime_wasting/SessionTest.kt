1869beb550e799f596ca2c0d7950a8bf51c474ad
meganerd151 <amylloyd151@gmail.com>
2021-08-15 19:27:56 +1200

    updated SessionTest.kt to test all of the setters within session

--- app/src/test/java/com/example/antitime_wasting/SessionTest.kt
+++ app/src/test/java/com/example/antitime_wasting/SessionTest.kt
@@ -2,6 +2,8 @@ package com.example.antitime_wasting
 
 import org.junit.Test
 import org.junit.Assert.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 /**
  * Class that tests to see if the correct values get saved to a session
@@ -14,9 +16,35 @@ class SessionTest {
         val et: Int = 5
         val s = Session(st, et, "Study")
 
+        //check to see if the initialization in this constructor works
         assertEquals(0, s.startTime)
         assertEquals(5, s.endTime)
         assertEquals("Study", s.sessionType)
         assertEquals(5, s.timeSpent)
+
+        //check to see if the setDate method works
+        val d: String = SimpleDateFormat("yyyy-MM-dd", Locale.US).format(Date())
+        s.setDate(d)
+        assertEquals(d, s.date)
+
+        //check to see if the setID method works
+        s.setID(10)
+        assertEquals(10, s.id)
+
+        //check to see if the setSessionType method works
+        s.setSessionType("Exercise")
+        assertEquals("Exercise", s.sessionType)
+
+        //check to see if the setStart method works
+        s.setStart(2)
+        assertEquals(2, s.startTime)
+
+        //check to see if the setEnd method works
+        s.setEnd(8)
+        assertEquals(8, s.endTime)
+
+        //check to see if the setTimeSpent method works
+        s.setTimeSpent(6)
+        assertEquals(6, s.timeSpent)
     }
 }
