3394c858e90a14c936bcf1d1009bff1ef429920e
Blake <macdadeblake@gmail.com>
2021-08-16 10:44:10 +1200

    Comments! in session.kt

--- app/src/main/java/com/example/antitime_wasting/Session.kt
+++ app/src/main/java/com/example/antitime_wasting/Session.kt
@@ -7,7 +7,8 @@ import java.util.*
 * @created 08/Aug/2021 - 2:20 PM
 * @project Anti time-wasting
 * @author Blake MacDade
-*/class Session {
+*/
+class Session {
 
     var id = 0
         private set
@@ -50,26 +51,50 @@ import java.util.*
         this.timeSpent = (end!! - start!!)
     }
 
+    /**
+     * Sets the id of the session
+     * @param ID id to assign
+     * */
     fun setID(ID:Int){
         this.id = ID
     }
 
+    /**
+     *  Sets the start time of the session
+     *  @param start the time to assign
+     * */
     fun setStart(start:Int){
         this.startTime = start
     }
 
+    /**
+     * Sets the end time of the session
+     * @param end the time to assign
+     * */
     fun setEnd(end:Int){
         this.endTime = end
     }
 
+    /**
+     * Sets the type of the session
+     * @param session the type to assign
+     * */
     fun setSessionType(session:String){
         this.sessionType = session
     }
 
+    /**
+     * Sets the date the session was started on
+     * @param date the date the assign
+     * */
     fun setDate(date: String){
         this.date = date
     }
 
+    /**
+     * Sets the total time spend on a session
+     * @param time the time spent to assign
+     * */
     fun setTimeSpent(time:Int){
         this.timeSpent = time
     }
