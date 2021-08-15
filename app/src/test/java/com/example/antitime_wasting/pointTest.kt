2e7a214e5b3d3f780882a7eeb7e17ed825969041
meganerd151 <amylloyd151@gmail.com>
2021-08-15 17:23:49 +1200

    change pointTest.kt

--- app/src/test/java/com/example/antitime_wasting/pointTest.kt
+++ app/src/test/java/com/example/antitime_wasting/pointTest.kt
@@ -3,17 +3,17 @@ package com.example.antitime_wasting
 import org.junit.Test
 import org.junit.Assert.*
 
-class pointTest {
+class PointTest {
     @Test fun makeAndChangePoint(){
         //makes point and checks to see if it was created correctly
         val p = Point(1, 2)
-        assertEquals(1, p.getx())
-        assertEquals(2, p.gety())
+        assertEquals(1, p.x)
+        assertEquals(2, p.y)
 
         //changes the values and checks to see if the changes held
         p.setx(4)
         p.sety(7)
-        assertEquals(4, p.getx())
-        assertEquals(7, p.gety())
+        assertEquals(4, p.x)
+        assertEquals(7, p.y)
     }
 }
