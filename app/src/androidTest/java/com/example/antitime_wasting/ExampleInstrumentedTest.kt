0c271d69db5f572e1b090c716270701788722151
Blake <macdadeblake@gmail.com>
2021-08-16 13:01:11 +1200

    trying to make codeacy happy

--- app/src/androidTest/java/com/example/antitime_wasting/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/antitime_wasting/ExampleInstrumentedTest.kt
@@ -16,6 +16,9 @@ import org.junit.Assert.*
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
     @Test
+    /**
+     * test context
+     * */
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
