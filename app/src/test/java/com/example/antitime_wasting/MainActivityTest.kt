90b71970aca5364792ba19db38a222b10e0ef167
Blake <macdadeblake@gmail.com>
2021-08-16 13:30:20 +1200

    added MainActivityTest.kt, not sure how to test though

---
+++ app/src/test/java/com/example/antitime_wasting/MainActivityTest.kt
@@ -0,0 +1,17 @@
+package com.example.antitime_wasting
+
+import org.junit.Test
+import org.junit.Assert.*
+import android.view.Menu
+
+/**
+* @created 16/Aug/2021 - 1:09 PM
+* @project Anti time-wasting
+* @author Blake MacDade
+*/
+class MainActivityTest {
+
+    @Test fun testOnCreate() {}
+    @Test fun testOnCreateOptionsMenu() {}
+    @Test fun testOnOptionsItemSelected() {}
+}
