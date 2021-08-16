af6ec7aaa224b4ea1f48419552a5c574634e75e9
Blake <macdadeblake@gmail.com>
2021-08-16 22:24:21 +1200

    readded testActivities

---
+++ app/src/androidTest/java/com/example/antitime_wasting/testActivities.kt
@@ -0,0 +1,81 @@
+package com.example.antitime_wasting
+
+import android.content.Context
+import androidx.lifecycle.Lifecycle
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.core.app.ActivityScenario
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+
+import org.junit.runner.RunWith
+import org.junit.Test
+import org.junit.Assert.*
+import org.junit.Rule
+import kotlin.jvm.Throws
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+
+/*
+* @created 16/Aug/2021 - 8:58 PM
+* @project Anti time-wasting
+* @author Blake MacDade
+*/
+/**
+ * Class to test activities
+ * */
+@RunWith(AndroidJUnit4::class)
+class testActivities {
+
+    /**
+     * Tests that the key components of MainActivity are there and working.
+     * */
+    @Test
+    @Throws(Exception::class)
+    fun testMainActivity(){
+        val mainActivityScenario = ActivityScenario.launch(MainActivity::class.java)
+
+        onView(withId(R.id.coordinator)).check(matches(isDisplayed()))
+        onView(withId(R.id.startButton)).check(matches(isDisplayed()))
+        onView(withId(R.id.mascot)).check(matches(isDisplayed()))
+        onView(withId(R.id.coordinator)).check(matches(isDisplayed()))
+    }
+
+    /**
+     * Tests that the key components of statsMenuActivity are there and working.
+     */
+    @Test
+    @Throws(Exception::class)
+    fun testStatsMenuActivity(){
+        val statsActivityScenario = ActivityScenario.launch(StatsMenuActivity::class.java)
+
+        onView(withId(R.id.statsConstraintLayout)).check(matches(isDisplayed()))
+        onView(withId(R.id.idGraphView)).check(matches(isDisplayed()))
+        onView(withId(R.id.view)).check(matches(isDisplayed()))
+        onView(withId(R.id.textView2)).check(matches(isDisplayed()))
+        onView(withId(R.id.sessionTypeSelector)).check(matches(isDisplayed()))
+        onView(withId(R.id.textView3)).check(matches(isDisplayed()))
+        onView(withId(R.id.scopeSelector)).check(matches(isDisplayed()))
+        onView(withId(R.id.applyBtn)).check(matches(isDisplayed()))
+    }
+
+    /**
+     * Tests that the key components of timeActivity are there and working.
+     */
+    @Test
+    @Throws(Exception::class)
+    fun testTimeMenuActivity(){
+        val timeActivityScenario = ActivityScenario.launch(TimeActivity::class.java)
+
+        onView(withId(R.id.timeConstraintLayout)).check(matches(isDisplayed()))
+        onView(withId(R.id.frameTimeActivity)).check(matches(isDisplayed()))
+        onView(withId(R.id.mascotTimeActivity)).check(matches(isDisplayed()))
+        onView(withId(R.id.timeText)).check(matches(isDisplayed()))
+        onView(withId(R.id.linearLayout)).check(matches(isDisplayed()))
+        onView(withId(R.id.createNewActivity)).check(matches(isDisplayed()))
+        onView(withId(R.id.linearLayout2)).check(matches(isDisplayed()))
+        onView(withId(R.id.studybtn)).check(matches(isDisplayed()))
+
+    }
+}
