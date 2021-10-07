0fe80ce6aa00402efb75081645ce83a962c9dc6a
Blake <macdadeblake@gmail.com>
2021-10-07 21:29:13 +1300

    moved to proper test directory

--- app/src/androidTest/java/com/timetric/antitime_wasting/MainUIelementsTest.kt
+++ app/src/androidTest/java/com/timetric/antitime_wasting/MainUIelementsTest.kt
@@ -1,4 +1,4 @@
-package com.example.antitime_wasting
+package com.timetric.antitime_wasting
 
 
 import android.view.View
@@ -10,6 +10,8 @@ import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.filters.LargeTest
 import androidx.test.rule.ActivityTestRule
 import androidx.test.runner.AndroidJUnit4
+import com.example.antitime_wasting.MainActivity
+import com.example.antitime_wasting.R
 import org.hamcrest.Description
 import org.hamcrest.Matcher
 import org.hamcrest.Matchers.allOf
